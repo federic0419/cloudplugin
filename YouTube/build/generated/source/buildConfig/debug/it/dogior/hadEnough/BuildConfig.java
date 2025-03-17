4a3bc690200f02d2fcd1eb4c8acaba5a83fd7a6d
federic0419 <fede20022004@gmail.com>
2025-03-17 12:44:18 +0100

    fatto il build

---
+++ YouTube/build/generated/source/buildConfig/debug/it/dogior/hadEnough/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package it.dogior.hadEnough;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "it.dogior.hadEnough";
+  public static final String BUILD_TYPE = "debug";
+}
