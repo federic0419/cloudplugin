4b01606dfbfdbc09979a9d6e3721611e2105375e
federic0419 <135590399+federic0419@users.noreply.github.com>
2025-03-18 12:45:28 +0100

    Update IPTVPlugin.kt

--- IPTV/src/main/kotlin/it/dogior/hadEnough/IPTVPlugin.kt
+++ IPTV/src/main/kotlin/it/dogior/hadEnough/IPTVPlugin.kt
@@ -1,13 +1,10 @@
 package com.lagradost
 
-import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
-import android.content.Context
+import com.lagradost.cloudstream3.plugin.Plugin
+import com.lagradost.cloudstream3.plugin.PluginManager
 
-@CloudstreamPlugin
-class FreeTVProviderPlugin: Plugin() {
-    override fun load(context: Context) {
-        // All providers should be added in this manner. Please don't edit the providers list directly.
-        registerMainAPI(FreeTVProvider())
+class IPTVPlugin : Plugin() {
+    override fun load() {
+        PluginManager.registerAPI(IPTV())
     }
 }
